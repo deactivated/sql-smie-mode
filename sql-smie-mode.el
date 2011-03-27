11bc2599c547addcccdb36942cd015cc7425df3c
mike spindel <deactivated@gmail.com>
2011-03-27 11:06:13 -0700

    add copyright notice

--- sql-smie-mode.el
+++ sql-smie-mode.el
@@ -1,3 +1,22 @@
+;;; sql-smie-mode.el --- Better indentation for sql-mode
+
+;; Copyright (c) 2011 Mike Spindel <deactivated@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'smie)
 
 (defvar sql-smie-indent-basic 4
@@ -104,3 +123,4 @@
 
 
 (provide 'sql-smie-mode)
+;;; sql-smie-mode.el ends here
